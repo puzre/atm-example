b7ba15c816210b9613e09a68923be9f7b20ce779
puzre <jose_antoniorivera@outlook.com>
2026-07-03 09:25:32 -0600

    feat: add Account implementation

--- domain/Account.cpp
+++ domain/Account.cpp
@@ -1,9 +1,41 @@
-//
-// Created by jose on 7/2/26.
-//
-
 #include "Account.h"
+#include <iostream>
+#include <utility>
 
 namespace domain
 {
-} // domain
+    Account::Account(std::string name, std::string code, const int pin, const double balance) : name_(std::move(name)),
+        code_(std::move(code)), pin_(pin), balance_(balance)
+    {
+    }
+
+    std::string Account::getName() const
+    {
+        return this->name_;
+    }
+
+    std::string Account::getCode() const
+    {
+        return this->code_;
+    }
+
+    int Account::getPin() const
+    {
+        return this->pin_;
+    }
+
+    double Account::getBalance() const
+    {
+        return this->balance_;
+    }
+
+    void Account::addBalance(const double balance)
+    {
+        this->balance_ += balance;
+    }
+
+    void Account::subtractBalance(const double balance)
+    {
+        this->balance_ -= balance;
+    }
+}
--- domain/Account.h
+++ domain/Account.h
@@ -5,13 +5,13 @@ namespace domain
 {
     class Account
     {
-        std::string name;
-        std::string code;
-        double balance = 0;
-        int pin;
+        std::string name_;
+        std::string code_;
+        int pin_;
+        double balance_ = 0;
 
     public:
-        Account(std::string& name, std::string& code, int pin, double balance);
+        Account(std::string  name, std::string  code, int pin, double balance);
         [[nodiscard]] std::string getName() const;
         [[nodiscard]] std::string getCode() const;
         [[nodiscard]] int getPin() const;
